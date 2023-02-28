f051e9dcad2bc0d49c12bf842d8d8e94147a7a26
Laure Matoussowsky <laurematoussowsky@gmail.com>
2023-02-28 14:45:53 +0100

    Created index for drags with index action in drag controller and index view, and added policies for pundit

--- app/controllers/drags_controller.rb
+++ app/controllers/drags_controller.rb
@@ -1,4 +1,6 @@
 class DragsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show new]
+
   def new
   end
 
@@ -6,9 +8,14 @@ class DragsController < ApplicationController
   end
 
   def index
+    @drags = Drag.all
+    @drags = policy_scope(Drag)
   end
 
   def show
+    raise
+    @drag = Drag.find(params[:id])
+    authorize @drag
   end
 
   def edit
