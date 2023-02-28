8e24fe80ab91a4b3c9377fd4ebdf2f1905f1503a
Shanti Ravdjee <shanti.ravdjee@gmail.com>
2023-02-28 13:04:22 +0100

    authorization in controllers & added devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   # Pundit: allow-list approach
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
@@ -12,6 +14,16 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+
+  def configure_permitted_parameters
+    attributes = [:first_name, :last_name]
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 
   private
 
