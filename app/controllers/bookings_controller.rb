cdc262931a3c4e47338d4a480763a78338c524f7
Shanti Ravdjee <shanti.ravdjee@gmail.com>
2023-03-01 18:45:52 +0100

    new-create-index methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,24 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    authorize @booking
   end
 
   def create
+    @booking = Booking.new(params_booking)
+    @booking.drag = params[:drag_id]
+    @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to bookings_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @bookings = Booking.all
+    @bookings = policy_scope(Booking)
   end
 
   def edit
@@ -15,5 +28,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:start_date, :end_date, :location)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :drag
   validates :end_date, comparison: { greater_than: :start_date }
   validates :location, presence: true
-
 end
