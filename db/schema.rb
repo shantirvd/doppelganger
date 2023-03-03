6d5ca14e8dab692a4c89bb9a7a52750243a1b5b4
Shanti Ravdjee <shanti.ravdjee@gmail.com>
2023-03-03 08:48:00 +0100

    added status column to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_135448) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_074505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_135448) do
     t.bigint "drag_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "booking_date"
+    t.string "status", default: "pending"
     t.index ["drag_id"], name: "index_bookings_on_drag_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
