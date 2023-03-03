b43fe762b251cd7d4db9687863d01b91757f4b29
Shanti Ravdjee <shanti.ravdjee@gmail.com>
2023-03-03 10:35:41 +0100

    added pending status to the booking dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:destroy]
   get "/dashboard", to: "pages#dashboard"
+  # patch "/dashboard", to: "drags#confirm"
+  # patch "/dashboard", to: "drags#decline"
 end
